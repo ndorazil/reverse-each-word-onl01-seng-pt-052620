dd4c8c376a17f674e9e1594e7a59d9ad941dd6ec
Nathan Dorazil <nathand1915@gmail.com>
2020-06-05 07:48:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  reversed_sentence = []
-  reversed_sentence = sentence.split
-  reversed_sentence.collect do |each_word|
-    reversed_sentence << each_word.reverse
-  end
-  return reversed_sentence.join(" ")
+  # reversed_sentence = []
+  # reversed_sentence = sentence.split
+  # reversed_sentence.collect do |each_word|
+  #   reversed_sentence << each_word.reverse
+  # end
+  # return reversed_sentence.join(" ")
 end
